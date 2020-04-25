584aa838bfe61fcf254252d4b430e19237131b7c
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2020-04-24 23:30:57 -0500

    feat: Add the CommandLineOption header stub

---
+++ inc/sys/CommandLineOption.h
@@ -0,0 +1,24 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: CommandLineOption.h
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2020 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef SYS_COMMANDLINEOPTION_H_
+#define SYS_COMMANDLINEOPTION_H_
+
+class CommandLineOption
+{
+  public:
+
+    CommandLineOption();
+
+    ~CommandLineOption();
+};
+#endif /* SYS_COMMANDLINEOPTION_H_ */
